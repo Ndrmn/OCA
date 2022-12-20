b39e928ac3d87157e81942f34aba75b8fe896e0e
Ndrmn <andrkult@gmail.com>
2022-12-20 22:58:25 +0100

    +galery with bugs

--- source/js/scriptdashboard.js
+++ source/js/scriptdashboard.js
@@ -12,6 +12,14 @@ async function getUsers() {
 
 	const table = document.querySelector('.users');
 
+	const galery = document.querySelector('.galery');
+	const image = document.querySelector('.galeryImage');
+	const btnX = document.querySelector('.closeBtn');
+
+	btnX.addEventListener('click', function() {
+		galery.classList.add('hideLoadCircle');
+	});
+
 	for (let i = 0; i < result.length; i++) {
 		table.insertAdjacentHTML(
 			'beforeend',
@@ -23,10 +31,18 @@ async function getUsers() {
 				<td>${result[i].phone}</td>
 				<td>${result[i].email}</td>
 				<td>${result[i].date_of_testing}</td>
-				<td><a class="btn btn-success" href="http://77.120.190.159:5555/api/get_test_graph/${result[i].id}" role="button">&#128200;</a></td>
+				<td><a class="btn btn-primary gal${result[i].id}" href="" role="button">&#128269;</a>
+				<a class="btn btn-success" href="http://77.120.190.159:5555/api/get_test_graph/${result[i].id}" role="button">&#129095;</a></td>
 			</tr>`
-		)
-	}
-}
+		);
+		
+		let btn = document.querySelector(`.gal${result[i].id}`);
+		btn.addEventListener('click', function() {
+			galery.classList.remove('hideLoadCircle');
+			image.src = `http://77.120.190.159:5555/api/get_test_graph/${result[i].id}`;
+		});
+	};
+};
 
 getUsers();
+
