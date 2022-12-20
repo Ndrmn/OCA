0fad6811555c097aa27c94275337bf60b4702242
Ndrmn <andrkult@gmail.com>
2022-12-20 22:59:30 +0100

    +galery

--- source/js/scriptdashboard.js
+++ source/js/scriptdashboard.js
@@ -31,7 +31,7 @@ async function getUsers() {
 				<td>${result[i].phone}</td>
 				<td>${result[i].email}</td>
 				<td>${result[i].date_of_testing}</td>
-				<td><a class="btn btn-primary gal${result[i].id}" href="" role="button">&#128269;</a>
+				<td><div class="btn btn-primary gal${result[i].id}" role="button">&#128269;</div>
 				<a class="btn btn-success" href="http://77.120.190.159:5555/api/get_test_graph/${result[i].id}" role="button">&#129095;</a></td>
 			</tr>`
 		);
